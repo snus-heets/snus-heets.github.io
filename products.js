17eada7106eec62a2451e56b77eaef18f0378ddd
snus-heets <snusandheets@gmail.com>
2026-05-05 23:25:02 -0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,37 @@
+const products = [
+
+/* BRAND 1 */
+{brand:1,id:1,name:"Product 1",price:5},
+{brand:1,id:2,name:"Product 2",price:4},
+{brand:1,id:3,name:"Product 3",price:6},
+
+/* BRAND 2 */
+{brand:2,id:1,name:"Product 1",price:3},
+{brand:2,id:2,name:"Product 2",price:7},
+
+/* BRAND 3 */
+{brand:3,id:1,name:"Product 1",price:2},
+{brand:3,id:2,name:"Product 2",price:5},
+
+/* BRAND 4 */
+{brand:4,id:1,name:"Product 1",price:8},
+
+/* BRAND 5 */
+{brand:5,id:1,name:"Product 1",price:6},
+
+/* BRAND 6 */
+{brand:6,id:1,name:"Product 1",price:4},
+
+/* BRAND 7 */
+{brand:7,id:1,name:"Product 1",price:5},
+
+/* BRAND 8 */
+{brand:8,id:1,name:"Product 1",price:9},
+
+/* BRAND 9 */
+{brand:9,id:1,name:"Product 1",price:3},
+
+/* BRAND 10 */
+{brand:10,id:1,name:"Product 1",price:7}
+
+];
